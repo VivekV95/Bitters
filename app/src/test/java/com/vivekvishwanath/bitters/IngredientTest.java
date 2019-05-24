de6f082a782c33ec592d141ce9d5bba7c0e9776b
VivekV95 <vivekv210@gmail.com>
2019-05-23 22:05:45 -0500

    Added unit tests for Ingredients Object construction

--- app/src/test/java/com/vivekvishwanath/bitters/IngredientTest.java
+++ app/src/test/java/com/vivekvishwanath/bitters/IngredientTest.java
@@ -1,4 +1,25 @@
 package com.vivekvishwanath.bitters;
 
+import com.vivekvishwanath.bitters.models.Cocktail;
+import com.vivekvishwanath.bitters.models.Ingredients;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class IngredientTest {
+
+    @Test
+    public void createIngredientsObject_WithEmptyConstructor() {
+        Ingredients ingredients = new Ingredients();
+        assertEquals(ingredients.getIngredientsId(), 0);
+    }
+
+    @Test
+    public void ingredientsIdCreated_WhenCocktailCreated() {
+        String id = "123";
+        Cocktail cocktail = new Cocktail(id);
+        Ingredients ingredients = cocktail.getIngredients();
+        assertEquals(ingredients.getIngredientsId(), Integer.parseInt(cocktail.getDrinkId()));
+    }
 }
