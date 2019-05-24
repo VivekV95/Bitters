62bd9e3aed92b2d8d954c8d0b06c1e623f4aa043
VivekV95 <vivekv210@gmail.com>
2019-05-24 00:33:34 -0500

    Added entry animation for cocktail cards

--- app/src/main/java/com/vivekvishwanath/bitters/adapters/CocktailListAdapter.java
+++ app/src/main/java/com/vivekvishwanath/bitters/adapters/CocktailListAdapter.java
@@ -15,6 +15,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -135,6 +137,12 @@ public class CocktailListAdapter extends RecyclerView.Adapter<CocktailListAdapte
                 }
             });
         }
+        setEnterAnimation(holder.cocktailParent);
+    }
+
+    public void setEnterAnimation(View viewToAnimate) {
+        Animation animation = AnimationUtils.loadAnimation(viewToAnimate.getContext(), android.R.anim.slide_in_left);
+        viewToAnimate.startAnimation(animation);
     }
 
     @Override
