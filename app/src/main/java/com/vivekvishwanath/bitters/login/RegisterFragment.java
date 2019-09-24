bcc22c95fcfc1baad002ec19ead4f031f31f91fd
VivekV95 <vivekv210@gmail.com>
2019-09-23 23:06:00 -0500

    Remove RegisterFragment listener

--- app/src/main/java/com/vivekvishwanath/bitters/login/RegisterFragment.java
+++ app/src/main/java/com/vivekvishwanath/bitters/login/RegisterFragment.java
@@ -20,7 +20,6 @@ import com.vivekvishwanath.bitters.apis.FirebaseAuthDao;
 
 public class RegisterFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
     private EditText editTextName;
     private EditText editTextEmail;
     private EditText editTextPassword;
@@ -87,40 +86,7 @@ public class RegisterFragment extends Fragment {
         buttonRegister.setOnClickListener(registerListener);
 
     }
-
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
-
+    
     private boolean checkFields() {
         if (TextUtils.isEmpty(editTextName.getText().toString())
                 || TextUtils.isEmpty(editTextEmail.getText().toString())
